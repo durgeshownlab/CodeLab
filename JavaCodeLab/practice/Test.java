50e5b87d878bead746c45af4a6924416840c7533
durgeshownlab <durgeshownlab@gmail.com>
2022-08-07 11:32:32 +0000

    java

--- JavaCodeLab/practice/Test.java
+++ JavaCodeLab/practice/Test.java
@@ -1,7 +1,11 @@
+import java.util.*;
 class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, world");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int a=sc.nextInt();
+        System.out.println(a+a);
     }
 }
